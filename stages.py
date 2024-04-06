4fdd2c915db1f7a8a1aee6cd452dc1f9ef40d42d
Linus Johansson <golfarenlj@gmail.com>
2024-04-06 19:13:29 +0000

    Stages & words added.

---
+++ stages.py
@@ -0,0 +1,56 @@
+def spell_stages(tries):
+    stages = [
+        """
+
+        (꧁ᐒ‗ᐘ꧂)
+
+        THATS IT! GAME OVER!
+
+        """,
+        
+        
+        """
+
+        (`益´)
+
+        1 more wrong and you are OUT!
+
+        """,
+        
+        
+        """
+
+        (⩺︷⩹)
+
+        Please not again!
+
+        """,
+        
+        
+        """
+
+        (⋋▂⋌)
+
+        Noooo!
+
+        """,
+        
+        
+        """
+
+        (눈_눈)
+
+        Wrong again...
+
+        """,
+        
+        
+        """
+
+        (¬､¬)
+
+        Wrong...
+
+        """,
+    ]
+    return stages[tries]
