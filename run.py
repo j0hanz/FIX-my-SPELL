8f06bf061bd8f3d957fbe1a020fe28dce2e17b8f
Linus Johansson <golfarenlj@gmail.com>
2024-04-06 20:04:23 +0000

    More words from fancytextpro added

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
+import word_art
 
+def main():
+    print(word_art.welcome)
+    print("Hello! ⊂(◉‿◉)つ".ljust(200))
+    
+    while True:
+        name = input("Enter your name please: ").capitalize()
+        if not name.isalpha():
+            print(f'Is that your name? Nah try again.\n')
+        else:
+            break
+main()
