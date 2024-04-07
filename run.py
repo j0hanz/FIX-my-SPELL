188a34fd384617e8e0d638471639cde9992ef76a
Linus Johansson <golfarenlj@gmail.com>
2024-04-07 15:48:43 +0000

    Added Thank you message, welcome message & updated user input

--- run.py
+++ run.py
@@ -2,7 +2,17 @@ import word_art
 import os
 import time
 import sys
-from stages import spell_stages
+
+
+def text_effect_fast(text):
+    for letter in text:
+        if letter == "\n":
+            print("\n")
+        else:
+            sys.stdout.write(letter)
+            sys.stdout.flush()
+            time.sleep(0.01)
+    print()
 
 
 def text_effect(text):
@@ -22,7 +32,7 @@ def text_effect(text):
 def clear_terminal():
     """
     Clears the terminal.
-    Credit: https://stackoverflow.com/questions/2084508/clear-terminal-in-python
+    https://stackoverflow.com/questions/2084508/clear-terminal-in-python
     """
     os.system("cls" if os.name == "nt" else "clear")
 
@@ -53,26 +63,59 @@ def play_again():
             return True
         if restart == "N":
             clear_terminal()
+            thank_you_message()
             main()
             return False
         else:
             print("Invalid choice. Please enter 'Y' or 'N'.\n")
 
 
-def main():
-    print(word_art.welcome)
-    print("Hello! ⊂(◉‿◉)つ".ljust(200))
-
-    while True:
-        name = input("Enter your name please: ").capitalize()
-        if not name.isalpha():
-            print(f"Is that your name? Nah try again.\n")
-        else:
-            break
-
+def thank_you_message():
+    text_effect("Thanks for playing!")
+    time.sleep(1)
+    text_effect_fast("Heading back to start menu...")
+    time.sleep(2)
     clear_terminal()
 
-    text_effect(f"Hello {name}!\nWelcome to FIX-my-SPELL!")
+
+def welcome_message():
+    print(word_art.welcome)
+    print("WELCOME TO FIX-my-SPELL!")
+    text_effect("Starting in 3 sec")
+    time.sleep(3)
+    clear_terminal()
+
+
+def user_input():
+    '''
+    Function to request a user's name and return it.
+    '''
+    user_name = ''
+    while True:
+        text_effect("What is your name?")
+        text_effect_fast("Press enter If you dont want to disclose your name")
+        user_name = input("").capitalize()
+        if user_name == "":
+            user_name = "User"
+            break
+        elif not user_name.isalpha():
+            clear_terminal()
+            print("(ᴗ˳ᴗ)".ljust(400))
+            text_effect(f"{user_name}?\nIs that really your name?")
+            text_effect("Nah try again.\n")
+        else:
+            break
+    return user_name
+
+
+def main():
+    welcome_message()
+    print(word_art.welcome)
+    user_name = user_input()
+    clear_terminal()
+    print("⊂(◉‿◉)つ".ljust(400))
+    text_effect(f"Hello {user_name}!\nWelcome to FIX-my-SPELL!")
+    time.sleep(1)
     while True:
         text_effect("Do you want to read the rules?")
         rules_input = input("(Y/N): ").upper()
@@ -81,8 +124,15 @@ def main():
             break
 
     while True:
-        text_effect("Press any key to start the game!  (✪‿✪) ")
+        text_effect(f"Press any key to start the game {user_name}!  (✪‿✪) ")
         input("")
+        text_effect("Starting in...")
+        print("3")
+        time.sleep(1)
+        print("2")
+        time.sleep(1)
+        print("1")
+        time.sleep(1)
 
         break
     clear_terminal()
